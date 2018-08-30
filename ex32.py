bde6e180ee327b525610587acf5015ef78a79a25
lokesh <lokesh.pathak@northout.com>
2018-08-30 15:23:02 +0530

    print pallindrom from file

--- ex32.py
+++ ex32.py
@@ -1,12 +1,12 @@
-# Write a version of a palindrome recogniser that accepts a file name from the user, 
+# Write a version of a palindrome recogniser that accepts a file name from the user,
 # reads each line, and prints the line to the screen if it is a palindrome.
 def palindrom():
 	punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~ '''
 
-	f=open('/home/tapfame/prog/palindrom','r+')
+	f=open('file_path','r+')
 	file=f.readlines()
 	#print file
-	
+
 
 	for line in file:
 		#import pdb; pdb.set_trace();
@@ -15,7 +15,7 @@ def palindrom():
 			#print char
 			if char in punctuations:
 				line = line.replace(char,'')
-		
+
 		if line.lower()==line[::-1].lower():
 			print True
 		else:
