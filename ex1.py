37d521a4ef78cc8496c86ae049840a574f1d077e
lokesh <lokesh.pathak@northout.com>
2018-08-30 13:02:01 +0530

    max of two number

---
+++ ex1.py
@@ -0,0 +1,12 @@
+# Define a function max() that takes two numbers as arguments and returns the largest of them.
+# Use the if-then-else construct available in Python.
+
+def max(num1, num2):
+  if num1 > num2:
+    return num1
+  else:
+    return num2
+
+#test
+print max(3, 5)
+print max(10, 6)
