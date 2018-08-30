f5ac0b7e6585903769252346951b083b7fad452a
lokesh <lokesh.pathak@northout.com>
2018-08-30 13:00:15 +0530

    convert doller to rupee

---
+++ doller_to_rupee.py
@@ -0,0 +1,10 @@
+"""
+To convert n doller to rupees based on cureent doller-rupeee ratio
+cureent rtion of doller to rupee is 70.74 Indian Rupee
+"""
+def convertor(n):
+	result=n * 70.74
+	return result
+#test
+# convert 5 Doller to rupee
+print convertor(5)
